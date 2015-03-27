26904805bd113da34640c7a1036695fbf6b4c2c7
Journeyman08 <jakecoltman@sky.com>
2015-03-27 16:13:27 +0000

    Working version

--- OOSSIS/OOSSIS/Program.cs
+++ OOSSIS/OOSSIS/Program.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Dts.Runtime;
+using Microsoft.SqlServer.Dts.Tasks;
+using System.Data;
+using Microsoft.SqlServer.Dts.Tasks.FileSystemTask;
+using Microsoft.SqlServer.Dts.Pipeline;
+using Microsoft.SqlServer.Dts.Pipeline.Wrapper;
+using Microsoft.SqlServer.Dts.Runtime.Wrapper;
+using Microsoft.SqlServer.Dts.Tasks.ScriptTask;
+
+
+
 
 namespace OOSSIS
 {
@@ -10,6 +21,23 @@ namespace OOSSIS
     {
         static void Main(string[] args)
         {
+
+            Microsoft.SqlServer.Dts.Runtime.Application app = new Microsoft.SqlServer.Dts.Runtime.Application();
+
+            SSISPackage package = new SSISPackage(@"C:\demo.dtsx");
+
+            SSISConnections.FlatFile filey = new SSISConnections.FlatFile(package, @"C:\temp.csv");
+
+            SSISConnections.FlatFileColumn col = new SSISConnections.FlatFileColumn(filey, 100, "PostCode");
+            SSISConnections.FlatFileColumn col2 = new SSISConnections.FlatFileColumn(filey, 100, "Count");
+
+            SSISTasks.File filetasky = new SSISTasks.File(package, "TestTask");
+            SSISTasks.Pipeline pipetask = new SSISTasks.Pipeline(package, "TestTaskPipe");
+
+            app.SaveToXml(package.get_location(), package.get_raw(), null);
+
         }
+
+
     }
 }
